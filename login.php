2be26c26512c1c1a9deef26e5eb3afba38213255
rai-pramana <rai.pramana46@gmail.com>
2022-12-22 11:10:40 +0800

    Changes to be committed: 	modified:   beranda-after.php 	modified:   jadwal.php 	modified:   login.php 	modified:   logout.php 	modified:   pengajuan.php 	modified:   pengumuman.php 	modified:   profile.php

--- login.php
+++ login.php
@@ -2,6 +2,12 @@
 
 session_start();
 
+// jika waktu session habis (tak set 30m)
+if (!isset($_SESSION['EXPIRES']) || time() >= $_SESSION['EXPIRES']) {
+    session_destroy();
+    $_SESSION = array();
+}
+
 if (isset($_SESSION["id_pegawai"])) { 
 
     header("location:beranda-after.php");
