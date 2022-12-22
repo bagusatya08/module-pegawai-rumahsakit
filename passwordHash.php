c5fc5b4c8d8fe3fae3fcbbf6a3d5fb3c3707a840
rai-pramana <rai.pramana46@gmail.com>
2022-12-22 08:50:09 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   passwordHash.php

--- passwordHash.php
+++ passwordHash.php
@@ -1,13 +1,13 @@
 <?php 
 
-require "dbConnection.php";
+require 'app/database/connect.php';
 
 $password_hash = password_hash("admin", PASSWORD_DEFAULT);
 
 $sql = "UPDATE tb_pegawai
-        SET password_pegawai = '$password_hash'
-        WHERE password_pegawai = 'admin'";
+        SET password_pg = '$password_hash'
+        WHERE password_pg = 'admin'";
 
-mysqli_query ($conn, $sql);
+mysqli_query($conn, $sql);
 
 ?>
