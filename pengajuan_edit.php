da00c5f6680b8145bf5e0dfe13534355ed80aac9
rai-pramana <rai.pramana46@gmail.com>
2023-01-05 22:27:47 +0800

    new file:   pengajuan_edit.php 	modified:   pengajuan_input.php

--- pengajuan_edit.php
+++ pengajuan_edit.php
@@ -12,40 +12,49 @@ if (!isset($_SESSION["id_pegawai"])) {
     header("location:login.php");
 
 } else {
-    $jenis_pengajuan = $_GET['jenis_pengajuan'];
+    $id_pengajuan = $_GET['id_pengajuan'];
 
     require './dbConnection.php';
+
+    $sql = "SELECT *
+        FROM tb_pengajuan
+        WHERE id_pengajuan = :id_pengajuan
+        ;
+    ";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id_pengajuan', $id_pengajuan, PDO::PARAM_STR);
+    $stmt->execute();
+    $pengajuan = $stmt->fetch(PDO::FETCH_ASSOC);
+
     
     if (isset($_POST['submit'])) {
         $konten = $_POST['konten'];
     
-        $media = $_FILES['media']['tmp_name'];
-        $pdf_blob = fopen($media, "rb");
+        if ($_FILES['media']['size'] != 0) {
+            $media = $_FILES['media']['tmp_name'];
+            $pdf_blob = fopen($media, "rb");
+        
+        } else {
+            $pdf_blob = $pengajuan['media'];
+    
+        }
     
         try {
             // input tb_pengajuan
-            $sql = "INSERT INTO tb_pengajuan(
-                            id_pegawai,
-                            jenis_pengajuan,
-                            tgl_masuk,
-                            konten,
-                            media,
-                            status_pengajuan
-                        )VALUES(
-                            :id_pegawai,
-                            :jenis_pengajuan,
-                            CURDATE(),
-                            :konten,
-                            :media,
-                            'N'
-                        );
+            $sql = "UPDATE tb_pengajuan
+                    SET
+                        tgl_masuk = CURDATE(),
+                        konten = :konten,
+                        media = :media
+                    WHERE 
+                        id_pengajuan = :id_pengajuan;
             ";
     
             $stmt = $pdo->prepare($sql);
-            $stmt->bindParam(':id_pegawai', $_SESSION["id_pegawai"]);
-            $stmt->bindParam(':jenis_pengajuan', $jenis_pengajuan);
             $stmt->bindParam(':konten', $konten);
             $stmt->bindParam(':media', $pdf_blob, PDO::PARAM_LOB);
+            $stmt->bindParam(':id_pengajuan', $id_pengajuan, PDO::PARAM_STR);
     
             if ($stmt->execute() === FALSE) {
                 echo 'Could not save information to the database';
@@ -69,7 +78,7 @@ if (!isset($_SESSION["id_pegawai"])) {
 <form method="POST" accept-charset="utf-8" enctype="multipart/form-data">
     <div>
         <label for="konten">Konten</label>
-        <input type="text" name="konten"/>
+        <input type="text" name="konten" value="<?= $pengajuan['konten']; ?>"/>
     </div>
     <div>
         <label for="media">File</label>
@@ -77,6 +86,6 @@ if (!isset($_SESSION["id_pegawai"])) {
         <input type="hidden" name="MAX_FILE_SIZE" value="67108864"/>
     </div>
     <div>
-        <input type="submit" name="submit" value="Buat pengajuan"/>
+        <input type="submit" name="submit" value="Edit pengajuan"/>
     </div>
 </form>
